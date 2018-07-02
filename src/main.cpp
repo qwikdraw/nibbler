a6a35441c87ee22ef4039db59b29783264d01d0f
theo-walton <theodore_walton@hotmail.com>
2018-07-01 21:40:23 -0700

    saving main

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,19 @@ int	main(int argc, char** argv)
 		exit(1);
 	}
 	(void)argv;
+
+	void *handle = dlopen(opengl/opengl.dylib, RTLD_LAZY | RTLD_LOCAL);
+	if (handle == NULL)
+	{
+		std::cout << "Bad thing" << std::endl;
+		exit(1);
+	}
+	IGraphic *graphic = reinterpret_cast<std::function<IGraphic*(glm::ivec2)>>dlsym(handle, "load_opengl");
+
+	if (graphic == NULL)
+	{
+		std::cout << "other Bad THING~!!!" << std::endl;
+		exit(1);
+	}	
 	return 0;
 }
