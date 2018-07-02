3a422510f2d607dddbe4bf9c6947a959644230ce
Logan James Kaser <logankaser@fyrn.io>
2018-07-01 20:58:07 -0700

    Improve usage

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@ int	main(int argc, char** argv)
 {
 	if (argc != 3)
 	{
-		std::cerr << "usage: ./nibbler <width> <height>\n where\n  width ∈ [low, high]\n  height ∈ [low, high]" << std::endl;
+		std::cerr
+			<< "usage: ./nibbler <width> <height>\n"
+			<< "where\n"
+			<< "  <width>  ∈ [low, high]\n"
+			<< "  <height> ∈ [low, high]" << std::endl;
 		exit(1);
 	}
 	(void)argv;
