582845f155d3a22c690245c1779669bf2b5c1257
theo-walton <theodore_walton@hotmail.com>
2018-07-02 23:44:38 -0700

    empty but compiling sfml class

--- sfml/src/Sfml.cpp
+++ sfml/src/Sfml.cpp
@@ -1,1 +1,31 @@
 #include "Sfml.hpp"
+
+extern "C" IGraphic	*load(glm::ivec2 mapsize)
+{
+	return new Sfml(mapsize);
+}
+
+Sfml::Sfml(glm::ivec2 mapsize)
+{
+	(void)mapsize;
+}
+
+void	Sfml::Draw(glm::ivec2 pos, char c)
+{
+	(void)pos;
+	(void)c;
+}
+
+void	Sfml::Display(void)
+{
+}
+
+struct Input	Sfml::Input(void)
+{
+	struct Input out = {false, false, false, false, false, false, false, false};
+	return out;
+}
+
+void	Sfml::Destroy(void)
+{
+}
--- sfml/src/Sfml.hpp
+++ sfml/src/Sfml.hpp
@@ -1,2 +1,16 @@
+#pragma once
+
 #include "../../src/IGraphic.hpp"
-#include <ncurses.h>
+#include <SFML/Graphics.hpp>
+
+class	Sfml : public IGraphic
+{
+public:
+
+	Sfml(glm::ivec2 mapsize);
+
+	void	Draw(glm::ivec2 pos, char c);
+	void	Display(void);
+	struct Input	Input(void);
+	void	Destroy(void);
+};
