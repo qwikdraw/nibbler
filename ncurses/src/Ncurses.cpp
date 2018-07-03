c6b1623a02f269896cee243510d3f6a5816418c5
theo-walton <theodore_walton@hotmail.com>
2018-07-02 23:09:03 -0700

    ncurses render looks nicer

--- ncurses/src/Ncurses.cpp
+++ ncurses/src/Ncurses.cpp
@@ -5,9 +5,14 @@ extern "C" IGraphic	*load(glm::ivec2 mapsize)
 	return new Ncurses(mapsize);
 }
 
+static void	draw_square(int y, int x)
+{
+	mvaddch(y, x * 2, ' ');
+	mvaddch(y, x * 2 + 1, ' ');
+}
+
 Ncurses::Ncurses(glm::ivec2 mapsize)
 {
-	(void)mapsize;
 	initscr();
 	timeout(0);
 	curs_set(0);
@@ -15,12 +20,49 @@ Ncurses::Ncurses(glm::ivec2 mapsize)
 	cbreak();
 	noecho();
 	nodelay(stdscr, TRUE);
-	start_color();			      
+	start_color();
+
+	init_color(COLOR_RED, 1000, 100, 500); // head color
+	init_color(COLOR_GREEN, 1000, 700, 7000); // body color
+	init_color(COLOR_BLUE, 1000, 100, 100); // food color
+	init_color(COLOR_YELLOW, 300, 1000, 1000); // wall color
+
+	init_pair(0, COLOR_WHITE, COLOR_BLACK); // default
+	init_pair(1, COLOR_WHITE, COLOR_RED);
+	init_pair(2, COLOR_WHITE, COLOR_GREEN);
+	init_pair(3, COLOR_WHITE, COLOR_BLUE);
+	init_pair(4, COLOR_WHITE, COLOR_YELLOW);
+	
+	attron(COLOR_PAIR(4));
+	for (int i = 0; i < mapsize.x + 2; i++)
+	{
+		draw_square(mapsize.y + 1, i);
+		draw_square(0, i);
+	}
+	for (int i = 0; i < mapsize.y + 2; i++)
+	{
+		draw_square(i, mapsize.x + 1);
+		draw_square(i, 0);
+	}
+	attroff(COLOR_PAIR(4));
 }
 
 void	Ncurses::Draw(glm::ivec2 pos, char c)
 {
-	mvaddch(pos.y, pos.x, c);
+	int palete = 0;
+	
+	if (c == 'O')
+		palete = 1;
+	else if (c == '#')
+		palete = 2;
+	else if (c == '@')
+		palete = 3;
+	else if (c == '%')
+		palete = 4;
+
+	attron(COLOR_PAIR(palete));
+	draw_square(pos.y + 1, pos.x + 1);
+	attroff(COLOR_PAIR(palete));
 }
 
 void	Ncurses::Display(void)
